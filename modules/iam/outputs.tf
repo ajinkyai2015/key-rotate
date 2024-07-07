99ee46bce350dd216189e967f33d45981d7cd50a
ajjju1993 <ajinkyaindulkar.005@gmail.com>
2024-07-06 21:00:59 -0500

    added output

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,3 +1,9 @@
-output "lambda_iam_role_arn" {
-  value = aws_iam_role.lambda_iam_role.arn
+output "iam_role_arn" {
+  description = "ARN of the IAM role created for Lambda"
+  value       = aws_iam_role.lambda_iam_role.arn
+}
+
+output "iam_policy_arn" {
+  description = "ARN of the IAM policy created for Lambda"
+  value       = aws_iam_policy.lambda_iam_policy.arn
 }
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,19 @@
-output "sns_topic_arn" {
-  value = module.sns.sns_topic_arn
+output "iam_role_arn" {
+  description = "ARN of the IAM role created for Lambda"
+  value       = module.iam.iam_role_arn
+}
+
+output "iam_policy_arn" {
+  description = "ARN of the IAM policy created for Lambda"
+  value       = module.iam.iam_policy_arn
 }
 
 output "lambda_function_arn" {
-  value = module.lambda.lambda_function_arn
+  description = "ARN of the Lambda function created"
+  value       = module.lambda.lambda_function_arn
+}
+
+output "sns_topic_arn" {
+  description = "ARN of the SNS topic created"
+  value       = module.sns.sns_topic_arn
 }
