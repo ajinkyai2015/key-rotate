f76a3bdc883072823c013ef4e9e0322b38f0b38b
ajjju1993 <ajinkyaindulkar.005@gmail.com>
2024-07-06 21:12:46 -0500

    commented output

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
-output "iam_role_arn" {
-  description = "ARN of the IAM role created for Lambda"
-  value       = module.iam.iam_role_arn
-}
+# output "iam_role_arn" {
+#   description = "ARN of the IAM role created for Lambda"
+#   value       = module.iam.iam_role_arn
+# }
 
-output "iam_policy_arn" {
-  description = "ARN of the IAM policy created for Lambda"
-  value       = module.iam.iam_policy_arn
-}
+# output "iam_policy_arn" {
+#   description = "ARN of the IAM policy created for Lambda"
+#   value       = module.iam.iam_policy_arn
+# }
 
 output "lambda_function_arn" {
   description = "ARN of the Lambda function created"
