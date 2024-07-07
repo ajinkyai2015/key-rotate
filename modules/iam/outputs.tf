8032c8f01f3c272eb7c132f46ed56a294d7c8aa2
ajjju1993 <ajinkyaindulkar.005@gmail.com>
2024-07-07 15:18:22 -0500

    Correct IAM role and outputs definition

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -3,15 +3,15 @@ output "lambda_iam_role_arn" {
   value       = aws_iam_role.lambda_iam_role.arn
 }
 
-output "lambda_iam_policy_arn" {
-  description = "ARN of the IAM policy created for Lambda"
-  value       = aws_iam_policy.lambda_iam_policy.arn
-}
+# output "lambda_iam_policy_arn" {
+#   description = "ARN of the IAM policy created for Lambda"
+#   value       = aws_iam_policy.lambda_iam_policy.arn
+# }
 
-output "readonly_role_arn" {
-  description = "The ARN of the read-only role"
-  value       = aws_iam_role.readonly.arn
-}
+# output "readonly_role_arn" {
+#   description = "The ARN of the read-only role"
+#   value       = aws_iam_role.readonly.arn
+# }
 
 output "readonly_user_access_key_id" {
   description = "The access key ID of the read-only user"
