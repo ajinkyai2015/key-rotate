a8509d12f4a1f14a1b0ac6eca2e4b008f480bb03
ajjju1993 <ajinkyaindulkar.005@gmail.com>
2024-07-07 13:29:47 -0500

    adding ec2 output in root

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,13 @@ output "sns_topic_arn" {
   description = "ARN of the SNS topic created"
   value       = module.sns.sns_topic_arn
 }
+
+output "ec2_instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = module.ec2.instance_id
+}
+
+output "ec2_instance_public_ip" {
+  description = "The public IP of the EC2 instance"
+  value       = module.ec2.instance_public_ip
+}
