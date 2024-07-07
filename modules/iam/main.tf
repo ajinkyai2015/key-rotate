b640337feaece3e2778d852fdd432c3857bbb05e
ajjju1993 <ajinkyaindulkar.005@gmail.com>
2024-07-07 15:05:16 -0500

    changing the IAM Policy

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,51 +1,5 @@
-resource "aws_iam_role" "lambda_iam_role" {
-  name = "lambda_iam_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_policy" "lambda_iam_policy" {
-  name   = "lambda_iam_policy_${terraform.workspace}"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = [
-          "iam:ListAccessKeys",
-          "iam:CreateAccessKey",
-          "iam:UpdateAccessKey",
-          "iam:DeleteAccessKey",
-        ],
-        Effect   = "Allow",
-        Resource = "*",
-      },
-      {
-        Action   = "sns:Publish",
-        Effect   = "Allow",
-        Resource = "*",
-      },
-    ],
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_iam_policy_attachment" {
-  role       = aws_iam_role.lambda_iam_role.name
-  policy_arn = aws_iam_policy.lambda_iam_policy.arn
-}
-
-
-####################RO ONLY Role to Test ###########################
-
 resource "aws_iam_role" "readonly" {
-  name = "readonly-role"
+  name = var.readonly_role_name
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -53,7 +7,7 @@ resource "aws_iam_role" "readonly" {
       {
         Effect = "Allow"
         Principal = {
-          Service = "ec2.amazonaws.com"
+          Service = "lambda.amazonaws.com"
         }
         Action = "sts:AssumeRole"
       }
@@ -61,22 +15,68 @@ resource "aws_iam_role" "readonly" {
   })
 }
 
-resource "aws_iam_policy_attachment" "readonly_policy" {
-  name       = "readonly-attachment"
-  roles      = [aws_iam_role.readonly.name]
-  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+resource "aws_iam_policy" "lambda_iam_policy" {
+  name        = "lambda_iam_policy"
+  description = "IAM policy for Lambda to manage IAM keys"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "iam:ListAccessKeys",
+          "iam:CreateAccessKey",
+          "iam:UpdateAccessKey",
+          "iam:DeleteAccessKey",
+          "iam:ListUsers",
+          "iam:GetAccessKeyLastUsed"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = "sns:Publish"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_iam_policy_attachment" {
+  role       = aws_iam_role.readonly.name
+  policy_arn = aws_iam_policy.lambda_iam_policy.arn
 }
 
 resource "aws_iam_user" "readonly_user" {
-  name = "readonly-user"
+  name = var.readonly_user_name
 }
 
 resource "aws_iam_access_key" "readonly_user_key" {
   user = aws_iam_user.readonly_user.name
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_iam_user" "test_user" {
+  name = "test-user"
+}
+
+resource "aws_iam_access_key" "test_user_key" {
+  user = aws_iam_user.test_user.name
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_secretsmanager_secret" "readonly_user_secret" {
-  name = "readonly-user-credentials"
+  name = var.secrets_manager_secret_name
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_secretsmanager_secret_version" "readonly_user_secret_version" {
@@ -85,4 +85,8 @@ resource "aws_secretsmanager_secret_version" "readonly_user_secret_version" {
     AccessKeyId     = aws_iam_access_key.readonly_user_key.id
     SecretAccessKey = aws_iam_access_key.readonly_user_key.secret
   })
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
