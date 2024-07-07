8f657bbc7d3f949e2a2de7ad0589930092df1dd3
ajjju1993 <ajinkyaindulkar.005@gmail.com>
2024-07-06 21:08:31 -0500

    satging output

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,9 +1,9 @@
-output "iam_role_arn" {
+output "lambda_iam_role_arn" {
   description = "ARN of the IAM role created for Lambda"
   value       = aws_iam_role.lambda_iam_role.arn
 }
 
-output "iam_policy_arn" {
+output "lambda_iam_policy_arn" {
   description = "ARN of the IAM policy created for Lambda"
   value       = aws_iam_policy.lambda_iam_policy.arn
 }
